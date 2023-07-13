b58723ac99c8e22121dd67317590af40a240e3e9
luksinocencio <luksinocencio@gmail.com>
2023-07-12 22:03:46 -0300

    *

--- BorutoServer/src/test/kotlin/com/example/ApplicationTest.kt
+++ BorutoServer/src/test/kotlin/com/example/ApplicationTest.kt
@@ -1,14 +1,23 @@
 package com.example
 
+import com.example.models.ApiResponse
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.server.testing.*
 import kotlin.test.*
 import io.ktor.http.*
 import com.example.plugins.*
+import com.example.repository.HeroRepository
+import com.example.repository.NEXT_PAGE_KEY
+import com.example.repository.PREVIOUS_PAGE_KEY
 import io.ktor.server.application.*
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import org.koin.java.KoinJavaComponent.inject
 
 class ApplicationTest {
+
+    private val heroRepository: HeroRepository by inject(HeroRepository::class.java)
     @Test
     fun `access root endpoint assert correct information`() {
         withTestApplication(moduleFunction = Application::module) {
@@ -20,6 +29,118 @@ class ApplicationTest {
                 assertEquals(
                     expected = "Welcome to Boruto API!",
                     actual = response.content
+                )
+            }
+        }
+    }
+
+    private fun calculatePage(page: Int): Map<String, Int?> {
+        var prevPage: Int? = page
+        var nextPage: Int? = page
+
+        if (page in 1..4) {
+            nextPage = nextPage?.plus(1)
+        }
+        if (page in 2..5) {
+            nextPage = nextPage?.plus(1)
+        }
+        if (page == 1) {
+            prevPage = null
+        }
+        if (page == 5) {
+            nextPage = null
+        }
+
+        return mapOf(PREVIOUS_PAGE_KEY to prevPage, NEXT_PAGE_KEY to nextPage)
+    }
+    @Test
+    fun `access all heroes endpoint, query all pages, assert correct information`() {
+        withTestApplication(moduleFunction = Application::module) {
+            val pages = 1..5
+            val heroes = listOf(
+                heroRepository.page1,
+                heroRepository.page2,
+                heroRepository.page3,
+                heroRepository.page4,
+                heroRepository.page5
+            )
+            pages.forEach { page ->
+                handleRequest (HttpMethod.Get, "/boruto/heroes?page=$page").apply {
+                    assertEquals(
+                        expected = HttpStatusCode.OK,
+                        actual = response.status()
+                    )
+                    val expected = ApiResponse(
+                        success = true,
+                        message = "ok",
+                        prevPage = calculatePage(page = page)["prevPage"],
+                        nextPage = calculatePage(page = page)["nextPage"],
+                        heroes = heroes[page - 1]
+                    )
+                    val actual = Json.decodeFromString<ApiResponse>(response.content.toString())
+                   assertEquals(
+                       expected = expected,
+                       actual = actual
+                   )
+                }
+            }
+        }
+    }
+    @ExperimentalSerializationApi
+    @Test
+    fun `acess all heroes endpoint, assert correct information`() {
+        withTestApplication(moduleFunction = Application::module) {
+            handleRequest(HttpMethod.Get, "/boruto/heroes").apply {
+                assertEquals(
+                    expected = HttpStatusCode.OK,
+                    actual = response.status()
+                )
+                val expected = ApiResponse(
+                    success = true,
+                    message = "ok",
+                    prevPage = null,
+                    nextPage = 2,
+                    heroes = heroRepository.page1
+                )
+
+                val actual = Json.decodeFromString<ApiResponse>(response.content.toString())
+
+                println("EXPECTED: $expected")
+                println("ACTUAL: $actual")
+
+                assertEquals(
+                    expected = expected,
+                    actual = actual
+                )
+            }
+        }
+    }
+
+    @ExperimentalSerializationApi
+    @Test
+    fun `acess all heroes endpoint, query second page, assert correct information`() {
+        withTestApplication(moduleFunction = Application::module) {
+            handleRequest(HttpMethod.Get, "/boruto/heroes?page=2").apply {
+                assertEquals(
+                    expected = HttpStatusCode.OK,
+                    actual = response.status()
+                )
+                val expected = ApiResponse(
+                    success = true,
+                    message = "ok",
+                    prevPage = 1,
+                    nextPage = 3,
+                    heroes = heroRepository.page2
+                )
+
+                val actual = Json.decodeFromString<ApiResponse>(response.content.toString())
+
+                println("EXPECTED: $expected")
+                println("ACTUAL: $actual")
+
+                assertEquals(
+                    expected = expected,
+                    actual = actual
                 )
             }
         }
