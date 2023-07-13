22aad46fae9b4844224a8319f3fc9508e7b47893
luksinocencio <luksinocencio@gmail.com>
2023-07-12 21:23:01 -0300

    *

--- BorutoServer/src/test/kotlin/com/example/ApplicationTest.kt
+++ BorutoServer/src/test/kotlin/com/example/ApplicationTest.kt
@@ -10,7 +10,7 @@ import io.ktor.server.application.*
 
 class ApplicationTest {
     @Test
-    fun accessRootEndpoint_AssertCorrectInformation() {
+    fun `access root endpoint assert correct information`() {
         withTestApplication(moduleFunction = Application::module) {
             handleRequest(HttpMethod.Get, "/").apply {
                 assertEquals(
