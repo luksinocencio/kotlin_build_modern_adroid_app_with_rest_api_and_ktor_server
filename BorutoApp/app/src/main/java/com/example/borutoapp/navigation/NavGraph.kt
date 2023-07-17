ac5b552c1408a45c0137a3ad05cabbe3871a6b01
luksinocecnio <luksinocencio@gmail.com>
2023-07-17 17:50:51 -0300

    *

--- BorutoApp/app/src/main/java/com/example/borutoapp/navigation/NavGraph.kt
+++ BorutoApp/app/src/main/java/com/example/borutoapp/navigation/NavGraph.kt
@@ -6,7 +6,7 @@ import androidx.navigation.NavType
 
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.navArgument
+import androidx.navigation.navArgument
 import com.example.borutoapp.util.Constants.DETAILS_ARGUMENT_KEY
 
 @Composable
