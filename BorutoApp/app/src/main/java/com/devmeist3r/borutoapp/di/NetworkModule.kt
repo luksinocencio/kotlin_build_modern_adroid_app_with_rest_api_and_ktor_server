2134126f3a5ac663ac98af1e60f64c95c14d7bcd
luksinocencio <luksinocencio@gmail.com>
2025-04-26 18:27:14 -0300

    *

---
+++ BorutoApp/app/src/main/java/com/devmeist3r/borutoapp/di/NetworkModule.kt
@@ -0,0 +1,48 @@
+package com.devmeist3r.borutoapp.di
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+import com.devmeist3r.borutoapp.data.remote.BorutoApi
+import com.devmeist3r.borutoapp.util.Constants.BASE_URL
+
+@ExperimentalSerializationApi
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .readTimeout(15, TimeUnit.MINUTES)
+            .connectTimeout(15, TimeUnit.MINUTES)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetofitInstance(okHttpClient: OkHttpClient): Retrofit {
+        val contentType = MediaType.get("application/json")
+
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(Json.asConverterFactory(contentType))
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideBorutoApi(retrofit: Retrofit): BorutoApi {
+        return retrofit.create(BorutoApi::class.java)
+    }
+}
--- BorutoApp/app/src/main/java/com/devmeist3r/borutoapp/util/Constants.kt
+++ BorutoApp/app/src/main/java/com/devmeist3r/borutoapp/util/Constants.kt
@@ -1,6 +1,8 @@
 package com.devmeist3r.borutoapp.util
 
 object Constants {
+    const val BASE_URL = "http://10.0.2.2:8080"
+
     const val DETAILS_ARGUMENT_KEY = "heroId"
 
     const val BORUTO_DATABASE = "boruto_database"
