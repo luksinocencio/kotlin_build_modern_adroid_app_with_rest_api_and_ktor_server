440eb184855439dad7011bfc0fe0fbdf8929a844
luksinocencio <luksinocencio@gmail.com>
2025-04-30 12:55:51 -0300

    *

---
+++ BorutoApp/app/src/androidTest/java/com/devmeist3r/borutoapp/data/paging_source/HeroRemoteMediatorTest.kt
@@ -0,0 +1,94 @@
+package com.devmeist3r.borutoapp.data.paging_source
+
+import androidx.paging.*
+import androidx.paging.RemoteMediator.*
+import androidx.test.core.app.ApplicationProvider
+import com.example.borutoapp.data.remote.FakeBorutoApi2
+import com.devmeist3r.borutoapp.data.local.BorutoDatabase
+import com.devmeist3r.borutoapp.domain.model.Hero
+import junit.framework.TestCase.assertFalse
+import junit.framework.TestCase.assertTrue
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+@ExperimentalCoroutinesApi
+class HeroRemoteMediatorTest {
+
+    private lateinit var borutoApi: FakeBorutoApi2
+    private lateinit var borutoDatabase: BorutoDatabase
+
+    @Before
+    fun setup() {
+        borutoApi = FakeBorutoApi2()
+        borutoDatabase = BorutoDatabase.create(
+            context = ApplicationProvider.getApplicationContext(),
+            useInMemory = true
+        )
+    }
+
+    @After
+    fun cleanup() {
+        borutoDatabase.clearAllTables()
+    }
+
+    @ExperimentalPagingApi
+    @Test
+    fun refreshLoadReturnsSuccessResultWhenMoreDataIsPresent() =
+        runBlocking {
+            val remoteMediator = HeroRemoteMediator(
+                borutoApi = borutoApi,
+                borutoDatabase = borutoDatabase
+            )
+            val pagingState = PagingState<Int, Hero>(
+                pages = listOf(),
+                anchorPosition = null,
+                config = PagingConfig(pageSize = 3),
+                leadingPlaceholderCount = 0
+            )
+            val result = remoteMediator.load(LoadType.REFRESH, pagingState)
+            assertTrue(result is MediatorResult.Success)
+            assertFalse((result as MediatorResult.Success).endOfPaginationReached)
+        }
+
+    @ExperimentalPagingApi
+    @Test
+    fun refreshLoadSuccessAndEndOfPaginationTrueWhenNoMoreData() =
+        runBlocking {
+            borutoApi.clearData()
+            val remoteMediator = HeroRemoteMediator(
+                borutoApi = borutoApi,
+                borutoDatabase = borutoDatabase
+            )
+            val pagingState = PagingState<Int, Hero>(
+                pages = listOf(),
+                anchorPosition = null,
+                config = PagingConfig(pageSize = 3),
+                leadingPlaceholderCount = 0
+            )
+            val result = remoteMediator.load(LoadType.REFRESH, pagingState)
+            assertTrue(result is MediatorResult.Success)
+            assertTrue((result as MediatorResult.Success).endOfPaginationReached)
+        }
+
+    @ExperimentalPagingApi
+    @Test
+    fun refreshLoadReturnsErrorResultWhenErrorOccurs() =
+        runBlocking {
+            borutoApi.addException()
+            val remoteMediator = HeroRemoteMediator(
+                borutoApi = borutoApi,
+                borutoDatabase = borutoDatabase
+            )
+            val pagingState = PagingState<Int, Hero>(
+                pages = listOf(),
+                anchorPosition = null,
+                config = PagingConfig(pageSize = 3),
+                leadingPlaceholderCount = 0
+            )
+            val result = remoteMediator.load(LoadType.REFRESH, pagingState)
+            assertTrue(result is MediatorResult.Error)
+        }
+}
--- BorutoApp/app/src/main/java/com/devmeist3r/borutoapp/data/local/BorutoDatabase.kt
+++ BorutoApp/app/src/main/java/com/devmeist3r/borutoapp/data/local/BorutoDatabase.kt
@@ -1,8 +1,10 @@
 package com.devmeist3r.borutoapp.data.local
 
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import android.content.Context
 import com.devmeist3r.borutoapp.data.local.dao.HeroDao
 import com.devmeist3r.borutoapp.data.local.dao.HeroRemoteKeysDao
 import com.devmeist3r.borutoapp.domain.model.Hero
@@ -12,7 +14,19 @@ import com.devmeist3r.borutoapp.domain.model.HeroRemoteKeys
 @TypeConverters(DatabaseConverter::class)
 abstract class BorutoDatabase : RoomDatabase() {
 
+    companion object {
+        fun create(context: Context, useInMemory: Boolean): BorutoDatabase {
+            val databaseBuilder = if (useInMemory) {
+                Room.inMemoryDatabaseBuilder(context, BorutoDatabase::class.java)
+            } else {
+                Room.databaseBuilder(context, BorutoDatabase::class.java, "test_database.db")
+            }
+            return databaseBuilder
+                .fallbackToDestructiveMigration()
+                .build()
+        }
+    }
+
     abstract fun heroDao(): HeroDao
     abstract fun heroRemoteKeysDao(): HeroRemoteKeysDao
-
 }
